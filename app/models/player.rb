07869435c4ec28609e9946ea55a61bd99156c34d
JonTDean <JonathanTomDean@Gmail.com>
2020-07-26 21:20:50 -0400

    Added Avatar

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ApplicationRecord
     has_many :user_players
     has_many :users, through: :user_players
+    has_many_attached :files
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@ class User < ApplicationRecord
     
     has_many :user_players
     has_many :players, through: :user_players
-
+    
+    # For Active Record
+    has_one_attached :avatar
+    
     # For Password Stuff--- BCrypt/JWT
     validates :username, presence: true, uniqueness: { case_senstive: false } # Force the User to make a UName 
 end
